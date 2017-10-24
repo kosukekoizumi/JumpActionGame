4a53b9bce0321af51b4581263df6c48d7094a0c9
kosukekoizumi <kosuke.koizumi@sankoushoji.co.jp>
2017-10-24 14:03:23 +0900

    最初のコミット

---
+++ core/src/jp/techacademy/kousuke/koizumi/jumpactiongame/ActivityRequestHandler.java
@@ -0,0 +1,8 @@
+package jp.techacademy.kousuke.koizumi.jumpactiongame;
+
+public interface ActivityRequestHandler {
+    public void showAds(boolean show);
+}
+
+// public class ActivityRequestHandler {
+// }
